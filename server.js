bb17bde40b6ca575ec49bb5699821a876be4ee65
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-08-16 10:48:15 -0400

    added .env and mongo cluster information

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = 3003
 // MIDDLEWARE
 APP.use(express.json())
 
+// MONGODB
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.j1xpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
 APP.listen(PORT, () => {
     console.log(`Listening to port: ${PORT}`)
 })
